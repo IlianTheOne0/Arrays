77113bd983269823fad9e473fce307c33bc26584
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-23 16:11:52 +0200

    Completed Task 1

--- main.cpp
+++ main.cpp
@@ -7,6 +7,16 @@ using std::cin;
 
 int main()
 {
+	unsigned long long income[6];
+	unsigned long long result = 0;
+
+	for (int i = 0; i <= 5; i++) {
+		cout << "Enter the income for " << (i + 1) << " month: ";
+		cin >> income[i];
+		result += income[i];
+	}
+
+	cout << "Result: " << result << endl;
 
 	_getch();
 	return 0;
