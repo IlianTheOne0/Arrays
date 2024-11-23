161d13f73cdc4c7c10fc8bb4a7502e68716bf481
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-23 16:36:26 +0200

    Completed Task 2

--- main.cpp
+++ main.cpp
@@ -7,6 +7,15 @@ using std::cin;
 
 int main()
 {
+	const unsigned int size = 10;
+	unsigned long long list[size] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+	cout << "Inverted array: " << endl;
+
+	for (int i = 9; i >= 0; i--)
+	{
+		cout << list[i] << endl;
+	}
 
 	_getch();
 	return 0;
