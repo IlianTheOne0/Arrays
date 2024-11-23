31c66470c7653bbe2ea79cc82b1a74e1c6f49c89
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-23 17:51:04 +0200

    Completed Task 1

--- main.cpp
+++ main.cpp
@@ -1,12 +1,63 @@
 #include <iostream>
 #include <conio.h>
+#include <random>
 
 using std::cout;
 using std::endl;
 using std::cin;
+using std::random_device;
+using std::mt19937;
+using std::uniform_int_distribution;
+
+int _max(unsigned long long* list)
+{
+    unsigned long long max = list[0];
+
+    for (int i = 0; i <= 9; i++)
+    {
+        if (list[i] >= max)
+        {
+            max = list[i];
+        }
+    }
+
+    return max;
+}
+
+int _min(unsigned long long* list)
+{
+    unsigned long long min = list[0];
+
+    for (int i = 0; i <= 9; i++)
+    {
+        if (list[i] <= min)
+        {
+            min = list[i];
+        }
+    }
+
+    return min;
+}
 
 int main()
 {
+	const int size = 10;
+	unsigned long long list[size];
+
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<> dist(1, 100);
+
+    cout << "Array: ";
+
+	for (int i = 0; i <= 9; i++)
+	{
+        list[i] = dist(rd);
+        cout << list[i] << " ";
+	}
+
+    cout << endl << endl << "Min: " << _min(list) << endl;
+    cout << "Max: " << _max(list) << endl;
 
 	_getch();
 	return 0;
