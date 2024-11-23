b3182f260a938266fab1c5d22d66a0c8333c5407
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-23 16:40:28 +0200

    Completed Task 3

--- main.cpp
+++ main.cpp
@@ -7,6 +7,18 @@ using std::cin;
 
 int main()
 {
+	unsigned long long length[5];
+	unsigned long long result = 0;
+
+	for (int i = 1; i <= 5; i++)
+	{
+		cout << "Enter the length of " << i << " side: ";
+		cin >> length[i];
+
+		result += length[i];
+	}
+
+	cout << "The perimeter of the pentagon: " << result << endl;
 
 	_getch();
 	return 0;
