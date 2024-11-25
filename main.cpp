1a5ddec777f4a8f556d78e2ea73d6f190efe76ff
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-25 22:11:10 +0200

    Started

--- main.cpp
+++ main.cpp
@@ -4,10 +4,58 @@
 using std::cout;
 using std::endl;
 using std::cin;
+using std::pair;
+
+pair<unsigned long long, int> _max(unsigned long long* list)
+{
+    unsigned long long max = list[0];
+    int month = 0;
+
+    for (int i = 1; i <= 11; i++)
+    {
+        if (list[i] >= max)
+        {
+            max = list[i];
+            month = i;
+        }
+    }
+
+    return { max, month };
+}
+
+pair<unsigned long long, int> _min(unsigned long long* list)
+{
+    unsigned long long min = list[0];
+    int month = 0;
+
+    for (int i = 1; i <= 11; i++)
+    {
+        if (list[i] <= min)
+        {
+            min = list[i];
+            month = i;
+        }
+    }
+
+    return { min, month };
+}
 
 int main()
 {
+    const unsigned int size = 12;
+    unsigned long long income[size];
 
-	_getch();
-	return 0;
+    for (int i = 0; i <= 11; i++) {
+        cout << "Enter the income for " << (i + 1) << " month: ";
+        cin >> income[i];
+    }
+
+    pair<unsigned long long, int> max_ = _max(income);
+    pair<unsigned long long, int> min_ = _min(income);
+
+    cout << "The highest income was in month " << (max_.second + 1) << ": " << max_.first << endl;
+    cout << "The lowest income was in month " << (min_.second + 1) << ": " << min_.first << endl;
+
+    _getch();
+    return 0;
 }
